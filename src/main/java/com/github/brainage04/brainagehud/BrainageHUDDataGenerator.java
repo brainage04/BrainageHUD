5ff866299e96339171262f7060e92441d1ee8cd5
brainage04 <thomasmcmah21@gmail.com>
2024-06-26 01:34:07 +1000

    Initial commit

---
+++ src/main/java/com/github/brainage04/brainagehud/BrainageHUDDataGenerator.java
@@ -0,0 +1,11 @@
+package com.github.brainage04.brainagehud;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BrainageHUDDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
