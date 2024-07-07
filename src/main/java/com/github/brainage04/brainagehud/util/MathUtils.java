b0ae4adeaf41d2144f7f5f2043974256b99d9ac8
brainage04 <thomasmcmah21@gmail.com>
2024-07-07 21:08:47 +1000

    added bit/byte utility functions

--- src/main/java/com/github/brainage04/brainagehud/util/MathUtils.java
+++ src/main/java/com/github/brainage04/brainagehud/util/MathUtils.java
@@ -17,4 +17,13 @@ public class MathUtils {
     public static String roundDecimalPlaces(double input, int decimalPlaces) {
         return roundDecimalPlaces((float) input, decimalPlaces);
     }
+
+    // from https://stackoverflow.com/a/9354899
+    public static byte getBit(int number, int position) {
+        return (byte) ((number >> position) & 1);
+    }
+
+    public static boolean isBitOn(int number, int position) {
+        return getBit(number, position) == 1;
+    }
 }
